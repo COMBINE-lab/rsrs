b9927cad89718f3f85c786036cca4f57445c7c1d
Rob Patro <rob.patro@gmail.com>
2024-03-30 21:43:22 -0400

    io types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use clap::{ArgGroup, Parser};
 use seqcol_rs::SeqCol;
+use std::io::Write;
 use std::path::{Path, PathBuf};
+
+#[derive(Debug, Clone)]
+pub enum OutputConfig {
+    Digest,
+    SeqColObj,
+    SeqColObjSNLP,
+}
+
+fn output_config_parser(s: &str) -> Result<OutputConfig, String> {
+    match s {
+        "digest" => Ok(OutputConfig::Digest),
+        "seqcol-obj" => Ok(OutputConfig::SeqColObj),
+        "seqcol-obj-snlp" => Ok(OutputConfig::SeqColObjSNLP),
+        t => Err(format!("Do not recognize output config {t}")),
+    }
+}
 
 /// Compute reference sequence digests
 /// according to the seqcol spec.
@@ -13,34 +30,67 @@ use std::path::{Path, PathBuf};
 ))]
 struct Args {
     /// Input FASTA file
-    #[arg(short, long)]
+    #[arg(long)]
     fasta: Option<PathBuf>,
 
     /// Input SAM/BAM file
-    #[arg(short, long)]
+    #[arg(long)]
     sam: Option<PathBuf>,
 
     /// Input SAM/BAM file
-    #[arg(short, long)]
+    #[arg(long)]
     seqcol: Option<PathBuf>,
+
+    /// Optional output path; if provided, output will be written
+    /// here rather than to stdout.
+    #[arg(short, long)]
+    out_path: Option<PathBuf>,
+
+    /// Type of output to produce, one of "digest", "seqcol-obj" or "seqcol-obj-snlp".
+    #[arg(short='t', long, default_value = "digest", value_parser = output_config_parser)]
+    output_type: OutputConfig,
 }
 
-fn process_fasta<P: AsRef<Path>>(fasta_path: P) -> anyhow::Result<String> {
+fn process_fasta<P: AsRef<Path>>(
+    fasta_path: P,
+    output_config: OutputConfig,
+) -> anyhow::Result<String> {
     let sc = SeqCol::try_from_fasta_file(fasta_path.as_ref())?;
-    let d = sc.digest(seqcol_rs::DigestConfig::default())?;
-    Ok(d)
+    match output_config {
+        OutputConfig::Digest => Ok(sc.digest(seqcol_rs::DigestConfig::default())?),
+        OutputConfig::SeqColObj => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::default())?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+        OutputConfig::SeqColObjSNLP => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::WithSeqnameLenPairs)?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+    }
 }
 
-fn process_seqcol<P: AsRef<Path>>(seqcol_path: P) -> anyhow::Result<String> {
+fn process_seqcol<P: AsRef<Path>>(
+    seqcol_path: P,
+    output_config: OutputConfig,
+) -> anyhow::Result<String> {
     let sf = std::fs::File::open(seqcol_path.as_ref())?;
     let r = std::io::BufReader::new(sf);
     let val = serde_json::from_reader(r)?;
     let sc = SeqCol::try_from_seqcol(&val)?;
-    let d = sc.digest(seqcol_rs::DigestConfig::default())?;
-    Ok(d)
+    match output_config {
+        OutputConfig::Digest => Ok(sc.digest(seqcol_rs::DigestConfig::default())?),
+        OutputConfig::SeqColObj => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::default())?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+        OutputConfig::SeqColObjSNLP => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::WithSeqnameLenPairs)?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+    }
 }
 
-fn process_sam<P: AsRef<Path>>(sam_path: P) -> anyhow::Result<String> {
+fn process_sam<P: AsRef<Path>>(sam_path: P, output_config: OutputConfig) -> anyhow::Result<String> {
     let mut reader = noodles_util::alignment::io::reader::Builder::default()
         .build_from_path(sam_path.as_ref())?;
     let header = reader.read_header()?;
@@ -50,37 +100,57 @@ fn process_sam<P: AsRef<Path>>(sam_path: P) -> anyhow::Result<String> {
             .iter()
             .map(|(k, v)| (k.as_slice(), v.length().into())),
     );
-    let d = sc.digest(seqcol_rs::DigestConfig::default())?;
-    Ok(d)
+    match output_config {
+        OutputConfig::Digest => Ok(sc.digest(seqcol_rs::DigestConfig::default())?),
+        OutputConfig::SeqColObj => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::default())?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+        OutputConfig::SeqColObjSNLP => {
+            let o = sc.seqcol_obj(seqcol_rs::DigestConfig::WithSeqnameLenPairs)?;
+            Ok(serde_json::to_string_pretty(&o)?)
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+
+    let mut out_stream: Box<dyn Write> = match &args.out_path {
+        Some(ref op) => std::fs::File::create(op).map(|f| Box::new(f) as Box<dyn Write>)?,
+        None => Box::new(std::io::stdout()),
+    };
 
     match args {
         Args {
             fasta: Some(fasta),
             sam: None,
             seqcol: None,
+            out_path: _,
+            output_type,
         } => {
-            let d = process_fasta(fasta)?;
-            println!("{d}");
+            let d = process_fasta(fasta, output_type)?;
+            writeln!(out_stream, "{d}")?;
         }
         Args {
             fasta: None,
             sam: Some(sam),
             seqcol: None,
+            out_path: _,
+            output_type,
         } => {
-            let d = process_sam(sam)?;
-            println!("{d}");
+            let d = process_sam(sam, output_type)?;
+            writeln!(out_stream, "{d}")?;
         }
         Args {
             fasta: None,
             sam: None,
             seqcol: Some(seqcol),
+            out_path: _,
+            output_type,
         } => {
-            let d = process_seqcol(seqcol)?;
-            println!("{d}");
+            let d = process_seqcol(seqcol, output_type)?;
+            writeln!(out_stream, "{d}")?;
         }
         _ => {}
     }
